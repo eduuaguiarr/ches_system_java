ecfd5c1eb5ed50767e9b19f93644d69f7480b06c
Eduardo Aguiar <eduardoneto5777@gmail.com>
2024-04-23 20:30:22 +0100

    chess layer and printing the board

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -4,11 +4,15 @@ public class Position {
 	private int row;
 	private int column;
 	
+	
+	
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
 
+	
+	
 	public int getRow() {
 		return row;
 	}
